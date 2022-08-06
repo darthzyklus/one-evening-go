1df0b7285f3adcafb5ebd9e57d142d6871a5dc68
Andres Uris <andres.urismontilla@gmail.com>
2022-08-05 22:28:54 -0400

    finish first http server exercise

---
+++ project-http-hello/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// Your solution goes here. Good luck!
+	http.HandleFunc("/hello", GreetHandler)
+
+	log.Println("Listen at port :8080")
+	err := http.ListenAndServe(":8080", nil)
+
+	log.Fatal(err)
+}
+
+func GreetHandler(writer http.ResponseWriter, req *http.Request) {
+	name := req.URL.Query().Get("name")
+	fmt.Fprint(writer, "Hello, ", name)
+}
