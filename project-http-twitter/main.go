ac5ac73d8fd9aeba5a2e163250ee987ea1188bd3
Andres Uris <andres.urismontilla@gmail.com>
2022-08-30 23:20:22 -0400

    resolve a few exercises about interfaces

--- project-http-twitter/main.go
+++ project-http-twitter/main.go
@@ -8,28 +8,25 @@ import (
 	"net/http"
 )
 
-var tweetCounter = 0
-
-func main() {
-	// Your solution goes here. Good luck!
-	http.HandleFunc("/tweets", tweetHandler)
-
-	//log.Println("Listen at port :8080")
-	err := http.ListenAndServe(":8080", nil)
-
-	log.Fatal(err)
+type TweetsRepository interface {
+	AddTweet(t Tweet) (int, error)
 }
 
-type Tweet struct {
-	Message  string `json:"message"`
-	Location string `json:"location"`
+type TweetsMemoryRepository struct {
+	tweets []Tweet
 }
 
-type response struct {
-	ID int `json:"ID"`
+func (repository *TweetsMemoryRepository) AddTweet(t Tweet) (int, error) {
+	repository.tweets = append(repository.tweets, t)
+
+	return len(repository.tweets), nil
 }
 
-func tweetHandler(writer http.ResponseWriter, req *http.Request) {
+type server struct {
+	tweetsRepository TweetsRepository
+}
+
+func (s server) addTweet(writer http.ResponseWriter, req *http.Request) {
 	body, err := io.ReadAll(req.Body)
 
 	if err != nil {
@@ -50,8 +47,16 @@ func tweetHandler(writer http.ResponseWriter, req *http.Request) {
 
 	fmt.Printf("Tweet: `%s` from %s\n", tweet.Message, tweet.Location)
 
+	id, err := s.tweetsRepository.AddTweet(tweet)
+
+	if err != nil {
+		log.Println("Failed to save the tweet:", err)
+		writer.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
 	payload, err := json.Marshal(response{
-		ID: tweetCounter + 1,
+		ID: id,
 	})
 
 	if err != nil {
@@ -59,6 +64,28 @@ func tweetHandler(writer http.ResponseWriter, req *http.Request) {
 		writer.WriteHeader(http.StatusInternalServerError)
 	}
 
-	tweetCounter++
 	writer.Write(payload)
+}
+
+func main() {
+	// Your solution goes here. Good luck!
+	s := server{
+		tweetsRepository: &TweetsMemoryRepository{},
+	}
+
+	http.HandleFunc("/tweets", s.addTweet)
+
+	//log.Println("Listen at port :8080")
+	err := http.ListenAndServe(":8080", nil)
+
+	log.Fatal(err)
+}
+
+type Tweet struct {
+	Message  string `json:"message"`
+	Location string `json:"location"`
+}
+
+type response struct {
+	ID int `json:"ID"`
 }
