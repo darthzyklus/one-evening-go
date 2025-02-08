182965a900e6807910f2c8cc8f7550cdac63de57
Andres Uris <andres.urismontilla@gmail.com>
2025-02-08 19:18:51 -0300

    complete 02-returning-errors exercise

---
+++ 06-errors/02-returning-errors/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func main() {
+	result, err := Divide(100, 50)
+	fmt.Println("Result:", result, "Error:", err)
+}
+
+func Divide(x float64, y float64) (float64, error) {
+	if y == 0 {
+		return 0, errors.New("division by zero is not allowed")
+	}
+
+	return x / y, nil
+}
