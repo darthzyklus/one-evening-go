b4dd6f2c9655fdd891d2ad3390679076f29da711
Andres Uris <andres.urismontilla@gmail.com>
2025-02-22 21:23:07 -0300

    add locks to twitter project

--- project-http-twitter/server/repository.go
+++ project-http-twitter/server/repository.go
@@ -1,5 +1,9 @@
 package server
 
+import (
+	"sync"
+)
+
 type TweetsRepository interface {
 	SaveTweet(tw Tweet) (int, error)
 	Tweets() ([]Tweet, error)
@@ -7,14 +11,21 @@ type TweetsRepository interface {
 
 type TweetsMemoryRepository struct {
 	tweets []Tweet
+	lock   sync.RWMutex
 }
 
 func (r *TweetsMemoryRepository) SaveTweet(tw Tweet) (int, error) {
+	r.lock.Lock()
+	defer r.lock.Unlock()
+
 	r.tweets = append(r.tweets, tw)
 
 	return len(r.tweets), nil
 }
 
 func (r *TweetsMemoryRepository) Tweets() ([]Tweet, error) {
+	r.lock.RLock()
+	defer r.lock.RUnlock()
+
 	return r.tweets, nil
 }
