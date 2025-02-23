b29a2ce77b5657e1867c58e5ce06fc63e95fb2e4
Andres Uris <andres.urismontilla@gmail.com>
2025-02-22 22:07:42 -0300

    spam tweets on twitter http server project

--- project-http-twitter/main.go
+++ project-http-twitter/main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"time"
+
+	"twitter/server"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
-
-import "twitter/server"
 
 func main() {
 	s := server.Server{
@@ -21,5 +26,60 @@ func main() {
 	router.Get("/tweets", s.ListTweets)
 	router.Post("/tweets", s.AddTweet)
 
+	go spamTweets()
+
 	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func spamTweets() {
+	for {
+		time.Sleep(time.Millisecond * 100)
+
+		// Prepare payload
+		payload := server.Tweet{
+			Message:  "ass",
+			Location: "ass",
+		}
+
+		// Marshal payload
+		payloadJSON, err := json.Marshal(payload)
+
+		if err != nil {
+			fmt.Printf("Failed to marshall payload: %v", err)
+			return
+		}
+
+		// Send HTTP POST Request
+		url := "http://localhost:8080/tweets"
+		contentType := "application/json"
+
+		resp, err := http.Post(url, contentType, bytes.NewBuffer(payloadJSON))
+
+		if err != nil {
+			fmt.Printf("Error at the HTTP POST request: %v", err)
+			return
+		}
+
+		// Defer close of the response  body
+		defer resp.Body.Close()
+
+		// read and print the response
+
+		body, err := io.ReadAll(resp.Body)
+
+		if err != nil {
+			fmt.Printf("Failed to read the response body: %v", err)
+			return
+		}
+
+		respBody := server.TweetResponse{}
+
+		if err := json.Unmarshal(body, &respBody); err != nil {
+			fmt.Printf("Failed to unmarshal response body: %v", err)
+			return
+		} else {
+			fmt.Printf("Added tweet %d\n", respBody.ID)
+		}
+
+	}
 }
